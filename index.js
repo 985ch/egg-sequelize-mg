33d515b48be6e5d8c69044356c5a7b2e1eb2f271
985ch <115839257@qq.com>
2019-07-07 01:14:31 +0800

    不再强依赖sequelize-auto，增加统计输出

--- index.js
+++ index.js
@@ -3,25 +3,30 @@
 const sequelizeGen = require('sequelize-mg');
 
 const readMysql = require('./lib/readmysql');
-const readDefault = require('./lib/readdefault');
 const defaultConfig = require('./lib/config');
 
-async function generate(config, tables, appRoot = './', reader = readDefault) {
+async function generate(config, tables, appRoot = './', reader) {
+  if (!reader)reader = require('./lib/readdefault');
+  let total = { ignore: 0, update: 0, create: 0 };
   if (!config.datasources) {
     const result = await reader(appRoot, config, tables);
-    await sequelizeGen(result.tables, result.info, result.config);
+    total = await sequelizeGen(result.tables, result.info, result.config);
   } else {
     tables = tables || {};
+
     for (const cfg of config.datasources) {
       const result = await reader(appRoot, cfg, tables[cfg.delegate]);
-      await sequelizeGen(result.tables, result.info, result.config);
+      const { ignore, update, create } = await sequelizeGen(result.tables, result.info, result.config);
+      total.ignore += ignore;
+      total.update += update;
+      total.create += create;
     }
   }
+  console.log(`ignore files:${total.ignore}\nupdate files:${total.update}\ncreate files:${total.create}`);
 }
 
 module.exports = {
   generate,
   readMysql,
-  readDefault,
   config: defaultConfig,
 };
